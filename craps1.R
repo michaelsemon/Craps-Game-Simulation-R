dd0a5de19765b23a95908a6765b267bbeb2f1033
michaelsemon <michaelsemon@michaels-mbp.attlocal.net>
2017-05-14 09:29:35 -0700

    new strat

--- craps1.R
+++ craps1.R
@@ -1,40 +1,54 @@
 # Global Simulations // No odds bets/other bets
-dice1 <- sample(1:6,1)
-dice2 <- sample(1:6,1)
-dice.roll <- dice1 + dice2
 passline.win <- c(7,11)
-passline.craps <- c(2,3,11)
+passline.craps <- c(2,3,12)
 passline.pass <- c(4,5,6,8,9,10,12)
 
 pass.bet <- 5 #Pass line bet
 stack <- 50 #starting cash
 
 #Shoot for the Point 
-point.roll <- function(point) {
-  roll <- dice.roll
-    if (roll == point) {
-        stack <- stack + pass.bet
-        print("hit the point")
-    } else if (roll %in% point.loss) {
-        stack <- stack - pass.bet
-    } else {
-        point.roll(point)
-    }
-}
 
+# point.roll <- function(point) {}
 
 #Simulate Craps 
 craps <- function() {
-  roll <- dice.roll  #pass roll
-  if(roll %in% passline.win) {  
+  initial.roll <- sample(1:6,1) + sample(1:6,1)
+  
+  if(initial.roll %in% passline.win) {  
     stack <- stack + pass.bet
-  } else if (roll %in% passline.craps) {
+    
+  } else if (initial.roll %in% passline.craps) {
       stack <- stack - pass.bet
+      
   } else {
-      point <- roll
-      point.roll(point)
+    point <- initial.roll 
+    roll <- sample(1:6,1) + sample(1:6,1)
+    
+    while ( !(roll %in% c(7,point)) ) {
+      roll <- dice.roll
+       
+        if (roll == point) {
+          stack <- stack + pass.bet
+          print("hit the point in while loop")
+          break
+          
+      } else if (roll == 7){
+          stack <- stack-pass.bet
+          print('7 out in while loop')
+      }
+    }
+    
+    if (roll == 7) {
+      stack <- stack - pass.bet
+      print("7 out")
+    } else if (roll == point) {
+        stack <- stack-pass.bet
+        print("hit the point initially")
+    }
+    
   }
+  print(stack)
 }
 
 
-
+craps()
